2fb049bd3d0858e82834badce902810fd6b7d07d
Rogger Matamoros <adn6691@gmail.com>
2014-02-23 22:33:19 -0500

    Update XposedMod.java

--- src/com/mohammadag/burnttoast/XposedMod.java
+++ src/com/mohammadag/burnttoast/XposedMod.java
@@ -32,8 +32,8 @@ public class XposedMod implements IXposedHookZygoteInit {
 				PackageManager pm = context.getPackageManager();
 
 				ImageView imageView = new ImageView(context);
-				imageView.setMaxHeight(view.getHeight() + 128);
-				imageView.setMaxWidth(view.getHeight() + 128);
+				imageView.setMaxHeight(view.getHeight() + 64);
+				imageView.setMaxWidth(view.getHeight() + 64);
 				imageView.setAdjustViewBounds(true);
 				imageView.setImageDrawable(pm.getApplicationIcon(context.getPackageName()));
 				LinearLayout.LayoutParams params1 = new LinearLayout.LayoutParams(
